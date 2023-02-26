07f7d288a188350464636150004ba26c75561105
LLpanov <leonardopanov@yahoo.com>
2023-02-26 04:11:50 +0200

    docker

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,5 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
+
 export class CreatePostDto {
+	@ApiProperty({ example: 'My car', description: 'some information about car' })
+	@IsString({ message: 'must be a string' })
 	readonly title: string;
+
+	@ApiProperty({ example: 'Content', description: 'its about user content' })
+	@IsString({ message: 'must be a string' })
 	readonly content: string;
+
+	@IsNumber({}, { message: 'must be a number' })
 	readonly userId: number;
 }
